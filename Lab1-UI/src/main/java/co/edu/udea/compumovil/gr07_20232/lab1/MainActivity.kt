bc4ab4e567b4f223a73eae01fcb178b850cbface
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-09-21 18:26:50 -0500

    Nueva clase para el display con los inputs del teclado

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20232/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20232/lab1/MainActivity.kt
@@ -3,16 +3,19 @@ package co.edu.udea.compumovil.gr07_20232.lab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.edu.udea.compumovil.gr07_20232.lab1.ui.PersonalDataActivity
+import co.edu.udea.compumovil.gr07_20232.lab1.ui.components.rememberImeState
 import co.edu.udea.compumovil.gr07_20232.lab1.ui.theme.LabsCM20232Gr07Theme
 
 enum class ScreenData(){
@@ -26,11 +29,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LabsCM20232Gr07Theme {
+                val imeState = rememberImeState()
+                val scrollState = rememberScrollState()
+                
+                LaunchedEffect(key1 = imeState.value){
+                    if(imeState.value){
+                        scrollState.animateScrollTo(scrollState.maxValue, tween(300))
+                    }
+                }
+                
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
-                        .verticalScroll(rememberScrollState()),
+                        .verticalScroll(scrollState),
                     color = MaterialTheme.colorScheme.primary
                 ) {
                     Main()
